b9c0b31f10c13539cbe02291a31f43184dd29d88
louie <louiecasula@gmail.com>
2023-11-07 09:37:52 -0500

    started on the tests for Cat and AnimalFactory

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,45 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Dog;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void createDogTest() {
+//        // Given
+//        String expectedName = "Genghis Khan";
+//        Date expectedBirthDate = 01/01/1162;
+//        Dog dog = new Dog(expectedName, expectedBirthDate);
+//
+//        // When
+//        String actualName = dog.getName();
+//        Date actualBirthDate = dog.getBirthDate();
+//
+//        // Then
+//        Assert.assertEquals(expectedBirthDate, actualBirthDate);
+//        Assert.assertEquals(expectedName, actualName);
+    }
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void createCatTest() {
+//        // Given
+//        String expectedName = "Genghis Khan";
+//        Date expectedBirthDate = 01/01/1162;
+//        Cat cat = new Cat(expectedName, expectedBirthDate);
+//
+//        // When
+//        String actualName = cat.getName();
+//        Date actualBirthDate = cat.getBirthDate();
+//
+//        // Then
+//        Assert.assertEquals(expectedBirthDate, actualBirthDate);
+//        Assert.assertEquals(expectedName, actualName);
+    }
 }
--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -3,6 +3,7 @@ package rocks.zipcodewilmington;
 import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
 
 import java.util.Date;
 
@@ -11,10 +12,71 @@ import java.util.Date;
  */
 public class CatTest {
     // TODO - Create tests for `void setName(String name)`
+    @Test
+    public void setNameTest() {
+        // Given (a name exists and a cat exists)
+        Cat cat = new Cat(null, null, null);
+        String givenName = "Meowy";
+
+        // When (a cat's name is set to the given name)
+        cat.setName(givenName);
+
+        // Then (we expect to get the given name from the cat)
+        String catName = cat.getName();
+        Assert.assertEquals(catName, givenName);
+    }
+
     // TODO - Create tests for `speak`
+    @Test
+    public void speakTest() {
+        // Given
+        Cat cat = new Cat("Meowy", null, null);
+        String expected = "meow!";
+
+        // When
+        String actual = cat.speak();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `setBirthDate(Date birthDate)`
+    @Test
+    public void setBirthDateTest() {
+        // Given (a name exists and a cat exists)
+        Cat cat = new Cat(null, null, null);
+        Date givenBirthDate = new Date();
+
+        // When (a cat's name is set to the given name)
+        cat.setBirthDate(givenBirthDate);
+
+        // Then (we expect to get the given name from the cat)
+        Date catBirthDate = cat.getBirthDate();
+        Assert.assertEquals(catBirthDate, givenBirthDate);
+    }
+
     // TODO - Create tests for `void eat(Food food)`
     // TODO - Create tests for `Integer getId()`
+    @Test
+    public void getIdTest() {
+        // Given
+        Cat cat = new Cat(null, null, null);
+        Cat cat1 = new Cat(null, null, 9007);
+
+        Integer expected = null;
+        Integer expected1 = 9007;
+
+
+        // When
+        Integer actual = cat.getId();
+        Integer actual1 = cat1.getId();
+
+
+        // Then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected1, actual1);
+    }
+
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 
